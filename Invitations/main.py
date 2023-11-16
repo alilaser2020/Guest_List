ad65a9cc513397644425b2ba357427d1c9cdb2ef
ali <alilaser8080@gmail.com>
2023-11-16 19:15:24 +0330

    Modify the main.py file again

--- Invitations/main.py
+++ Invitations/main.py
@@ -19,3 +19,25 @@ if __name__ == "__main__":
             invitations[i] = "mohammad"
             break
     print(f"Invitations: {invitations}")
+    print("----------------------------------------------------------------------")
+    # Version 3: (more guests)
+    invitations = ["ali", "ahmad", "mahdi"]
+    print("***Welcome to invite to our dinner!***")
+    for x in invitations:
+        print(f"*Hello {x.title()}, you're invited to our dinner*")
+
+    moreGuests = ["mohammad", "amir", "parsa"]
+    for x in moreGuests:
+        print(f"Attention! {x.title()} informed that he wants to be invited to dinner.")
+
+    for i in range(len(moreGuests)):
+        if i == 0:
+            invitations.insert(0, moreGuests[i])
+        elif i == 1:
+            invitations.insert(1, moreGuests[i])
+            # invitations.insert(2, moreGuests[i])
+        else:
+            invitations.append(moreGuests[i])
+
+    for x in invitations:
+        print(f"*Hello {x.title()}, you're invited to our dinner*")
