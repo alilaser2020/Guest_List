1b2bea370557a85bbee8fcd867b05e52c4204b0e
ali <alilaser8080@gmail.com>
2023-11-16 19:03:44 +0330

    add Invitations folder

---
+++ Invitations/main.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    # Version 1:
+    invitations = ["ali", "ahmad", "mahdi"]
+    print("***Welcome to invite to our dinner!***")
+    for x in invitations:
+        print(f"*Hello {x.title()}, you're invited to our dinner*")
