5912f38d4ab0cfc0427f838d164f40c06e6ffe16
ali <alilaser8080@gmail.com>
2023-11-19 15:16:46 +0330

    last modified main.py file

--- Invitations/main.py
+++ Invitations/main.py
@@ -41,3 +41,24 @@ if __name__ == "__main__":
 
     for x in invitations:
         print(f"*Hello {x.title()}, you're invited to our dinner*")
+
+    print("----------------------------------------------------------------------")
+    # Version 4: (special guests)
+    print("***Welcome to invite to our dinner!***")
+    print(invitations)
+    print(f"Sorry everyone, I can invite only two people {invitations[1].title()} and {invitations[3].title()} "
+          f"for dinner")
+    s1 = invitations[1]
+    s2 = invitations[3]
+    for i in range(len(invitations)):
+        p = invitations.pop(0)
+        if p == s1 or p == s2:
+            print(f"*Welcome {p.title()}, you’re still invited to dinner*")
+        else:
+            print(f"I'm sorry {p.title()} that I can’t invite you to dinner!")
+
+    # print(f"invitations = {invitations}")
+    # for x in invitations:
+    #     del x
+    #
+    # print(invitations)
